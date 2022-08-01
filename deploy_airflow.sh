3d58eeaa276af521282d08e3be101fb1e69c47a7
seu1ki <skkim@dgist.ac.kr>
2022-08-01 04:25:46 +0000

    edit deploy path

--- deploy_airflow.sh
+++ deploy_airflow.sh
@@ -1,4 +1,5 @@
-mkdir -p ./dags ./logs ./plugins
-echo -e "AIRFLOW_UID=$(id -u)" > .env
-docker-compose up airflow-init
+cd /home/ec2-user/deploy &&
+mkdir -p ./dags ./logs ./plugins &&
+echo -e "AIRFLOW_UID=$(id -u)" > .env &&
+docker-compose up airflow-init &&
 docker-compose up
